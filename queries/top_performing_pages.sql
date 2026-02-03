9959cd58bb68ec9df0719ccdda7962578ff62b79
Kaustubh <147119055+kaustubh-ds@users.noreply.github.com>
2026-02-03 17:07:51 +0530

    Pull top performing webpages

---
+++ queries/top_performing_pages.sql
@@ -0,0 +1,14 @@
+SELECT
+  -- Extracting clean URL and Title from nested GA4 data structure
+  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_location') AS page_url,
+  (SELECT value.string_value FROM UNNEST(event_params) WHERE key = 'page_title') AS page_title,
+  COUNT(*) AS total_page_views,
+  COUNT(DISTINCT user_pseudo_id) AS unique_visitors
+FROM `ever-lasting-analytics.analytics_264923661.events_*`
+WHERE event_name = 'page_view'
+  AND _TABLE_SUFFIX BETWEEN 
+    FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL EXTRACT(DAYOFWEEK FROM CURRENT_DATE()) + 5 DAY)) 
+    AND FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL EXTRACT(DAYOFWEEK FROM CURRENT_DATE()) - 1 DAY))
+GROUP BY 1, 2
+ORDER BY 3 DESC
+LIMIT 10
